887605c420fc07484652b68f394fbe6a3d921b35
abhishekverma1996 <43173187+abhishekverma1996@users.noreply.github.com>
2022-08-15 20:30:35 +0530

    Create Simple Sum.java

---
+++ Operators, Control Structures - In Class/Simple Sum.java
@@ -0,0 +1,17 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      int A,B,C;
+                      Scanner abhi= new Scanner(System.in);
+                      A= abhi.nextInt();
+                      B= abhi.nextInt();
+                      C= abhi.nextInt();
+                      System.out.println(A+B+C);
+                      
+                      
+    }
+}
