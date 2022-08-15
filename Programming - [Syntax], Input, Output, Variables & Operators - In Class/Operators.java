d35b16c91198e2d805bc01bf412ec2c0d72a6f40
abhishekverma1996 <43173187+abhishekverma1996@users.noreply.github.com>
2022-08-15 20:25:40 +0530

    Create Operators.java

---
+++ Programming - [Syntax], Input, Output, Variables & Operators - In Class/Operators.java
@@ -0,0 +1,19 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+                    
+    }
+}
