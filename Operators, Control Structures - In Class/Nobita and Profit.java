697dc95fa0347cdc147525cc93fdfc77cd8d388f
abhishekverma1996 <43173187+abhishekverma1996@users.noreply.github.com>
2022-08-15 20:31:10 +0530

    Create Nobita and Profit.java

---
+++ Operators, Control Structures - In Class/Nobita and Profit.java
@@ -0,0 +1,14 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      int C,S;
+        Scanner abhi=new Scanner(System.in);
+        C= abhi.nextInt();
+        S=abhi.nextInt();
+        System.out.println(S-C);
+    }
+}
