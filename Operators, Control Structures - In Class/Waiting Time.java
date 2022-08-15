e57cdf1919e7a8c934b2ea0044209de057c7b75c
abhishekverma1996 <43173187+abhishekverma1996@users.noreply.github.com>
2022-08-15 20:32:17 +0530

    Create Waiting Time.java

---
+++ Operators, Control Structures - In Class/Waiting Time.java
@@ -0,0 +1,14 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+            int N,X;
+            Scanner abhi= new Scanner(System.in);
+            N=abhi.nextInt();
+            X=abhi.nextInt();
+            System.out.println(N*X);
+    }
+}
