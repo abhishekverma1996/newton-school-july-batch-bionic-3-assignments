13c1c02c3d4d6a5b4e9d60b527e1c3e2b77ddd61
abhishekverma1996 <43173187+abhishekverma1996@users.noreply.github.com>
2022-08-15 20:40:58 +0530

    Create Sara and pattern (Functional Problem).java

---
+++ Ternary Operator, Loops - In Class/Sara and pattern (Functional Problem).java
@@ -0,0 +1,11 @@
+static void Pattern(int N){
+for(int i=0;i<N;i++){
+    int t = i * 6;
+        for(int j=0;j<N;j++){
+            int k = j*4;
+                System.out.print((t+k)+" ");
+        }
+                System.out.println(" ");
+            }
+    
+}
