9299620f457b48c745da04272fdc86846d84f55e
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2022-09-21 11:32:23 +0530

    Create Longest Consecutive Sequence.java

---
+++ Hashing - Post Class/Longest Consecutive Sequence.java
@@ -0,0 +1,41 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num-1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum+1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+    public static void main (String[] args) {
+                      // Your code here
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(longestConsecutive(arr));
+    }
+}
